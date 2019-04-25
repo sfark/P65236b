d09a6e23e2ad90bdff462cd49ce374d2d499f7b8
sfark <soerenfaerk@gmail.com>
2019-04-25 08:30:53 +0200

    ui

--- DATA/ADL.R
+++ DATA/ADL.R
@@ -2,8 +2,8 @@ end = length(X_t)
 lagmaks <- 25
 
 
-lagseq <- seq(1,lagmaks)
-laqmod <- seq(lagmaks,1)
+lagseq <- seq(2,lagmaks)
+laqmod <- seq(lagmaks,2)
 {
 price_train = c()
   #X_t[2:(end-(lagmaks+1))]
@@ -26,7 +26,7 @@ rain_train = c()
 namerain_train <- c()
 
 
-for (i in 1:(lagmaks)) {
+for (i in 1:(lagmaks-1)) {
   price_train <-cbind(price_train, X_t[lagseq[i]:(end-laqmod[i])])
   nameprice_train<- c(nameprice_train,paste("price_train_l",toString(i),sep = ""))
   
@@ -47,6 +47,7 @@ colnames(hydro_train) <- namehydro_train
 colnames(consumption_train) <- nameconsumption_train
 colnames(temp_train) <- nametemp_train
 colnames(rain_train) <- namerain_train
+datalag <- cbind(price_train,hydro_train,consumption_train,temp_train,rain_train)
 }
 {
 #Normal OLS setup, including all lags of exo. var.
@@ -109,7 +110,7 @@ for (i in 1:lagtjek) {
    
 }
 }
-par(mfrow=c(1,1))
+par(mfrow=c(2,1))
 plot(decompose(ts(data_NO1[,2],frequency = 365))$random)
 
 
@@ -144,12 +145,12 @@ auto.arima(hydro_sason,max.p = 10,max.q = 10,max.order = 100)#hydro
 
 aa_hydro <- auto.arima(hydro_sason,max.p = 10,max.q = 10,max.order = 100)#hydro
 hydro_coef <- aa_hydro$coef[1:10] 
-arma55model = auto.arima(hydro_sason,max.p = 10,max.q = 10,max.order = 100)
+arma55model = aa_hydro
 arma55model
 pwx=arma55model$residuals
 newpwy = stats::filter(X_t, filter = as.numeric(hydro_coef), sides =1)
 ccf (newpwy,pwx,na.action=na.omit,lag.max = 300)
-
+hydropre <- prewhiten(data_NO1[,2],X_t,x.model=arma55model,lag.max=365)
 
 
 aa_con <- auto.arima(hydro_sason)#consump
@@ -157,8 +158,10 @@ consum_coef <- c(1/(-aa_con$coef[5]),aa_con$coef[1]+1/(-aa_con$coef[5]),aa_con$c
 arma1model = auto.arima(data_NO1[,3]/(1000))
 arma1model
 pwx=arma1model$residuals
-newpwy = stats::filter(X_t, filter = as.numeric(consum_coef), sides =1)
-ccf (newpwy,pwx,na.action=na.omit)
+newpwy = Arima(X_t,model= arma1model)
+ccf (newpwy$residuals,pwx,na.action=na.omit)
+prewhiten(data_NO1[,3],X_t,x.model=arma1model)
+conpre <- prewhiten(data_NO1[,3],X_t,x.model=arma1model)
 
 
 aa_temp <- auto.arima(data_NO1[,4])#temp
@@ -168,6 +171,16 @@ arma5model
 pwx=arma5model$residuals
 newpwy = stats::filter(X_t, filter = as.numeric(temp_coef), sides =1)
 ccf (newpwy,pwx,na.action=na.omit)
+prewhiten(data_NO1[,4],X_t,x.model=arma5model,lag.max=365)
+temppre <- prewhiten(data_NO1[,4],X_t,x.model=arma5model,lag.max=365)
+
+for (i in 1:length(temppre$ccf)) {
+  
+}
+
+
+
+
 
 aa_rain <- auto.arima(data_NO1[,5])#rain
 rain_coef <- c(-1/(aa_rain$coef[2]),(aa_rain$coef[1]+1)/aa_rain$coef[2],(-aa_rain$coef[1])/aa_rain$coef[2])
@@ -176,12 +189,75 @@ arma11model
 pwx=arma11model$residuals
 newpwy = stats::filter(X_t, filter = as.numeric(rain_coef), sides =1)
 ccf (newpwy,pwx,na.action=na.omit)
+prewhiten(data_NO1[,5],X_t,x.model=arma11model,lag.max=365)
+rainpre <- prewhiten(data_NO1[,5],X_t,x.model=arma11model,lag.max=365)
 
+acf(X_t)
+acf(X_t,family="serif")
+aa_price <- auto.arima(data_NO1[,1])#price
 
+arma11model = auto.arima(data_NO1[,1])
+arma11model
+plot(arma11model$residuals)
+prewhiten(data_NO1[,1],X_t,x.model=arma11model)
+rainpre <- prewhiten(data_NO1[,5],X_t,x.model=arma11model,lag.max=365)
 # con lag 2,3,4,5
 # temp lag 1, 2, 3, 4, 5
 # rain lag 1
+rainpre_lag <- c()
+temppre_lag <- c()
+hydropre_lag <- c()
+conpre_lag <- c()
+laqseq <- seq(-100,-1)
+for (i in 1:length(laqseq)) {
+  if (rainpre$ccf[laqseq[i]]< -0.05 || rainpre$ccf[laqseq[i]]> 0.05) {
+    rainpre_lag <- c(rainpre_lag,laqseq[i])
+  }
+  if (temppre$ccf[laqseq[i]]< -0.05 || temppre$ccf[laqseq[i]]> 0.05) {
+    temppre_lag <- c(temppre_lag,laqseq[i])
+  }
+  if (hydropre$ccf[laqseq[i]]< -0.05 || hydropre$ccf[laqseq[i]]> 0.05) {
+    hydropre_lag <- c(hydropre_lag,laqseq[i])
+  }
+  if (conpre$ccf[laqseq[i]]< -0.05 || conpre$ccf[laqseq[i]]> 0.05) {
+    conpre_lag <- c(conpre_lag,laqseq[i])
+  }
+}
 
+rainpre_lag
+temppre_lag 
+hydropre_lag 
+conpre_lag 
+
+rainpre_lag <- rainpre_lag*-1
+temppre_lag <- temppre_lag *-1
+hydropre_lag <- hydropre_lag *-1
+conpre_lag <- conpre_lag *-1
+
+hydro_train[,hydropre_lag]
+consumption_train[,conpre_lag]
+temp_train[,temppre_lag]
+rain_train[,conpre_lag]
+
+testmodel <- lm(X_t[1:(length(X_t)-lagmaks-1)]~-1+price_train[,c(1,2, 3,  6, 13, 14)]+hydro_train[,c(4)]+consumption_train[,c(1,2,10,11,15,16)]+temp_train[,c(1,17)]+rain_train[,c(2,5,6)])
+testmodel <- lm(X_t[1:(length(X_t)-lagmaks)]~-1+datalag)
+AIC(testmodel)
+testsum <- summary(testmodel)
+testsum$coefficients[,4]
+goodlag <- c()
+pvallag <- sort(testsum$coefficients[,4], decreasing = FALSE) 
+for (i in 1:(lagmaks-1)) {
+    if (pvallag[i]<0.05) {
+      goodlag <- c(goodlag,names(pvallag[i]))
+    }
+}
+
+sort(goodlag)
+
+
+dim(price_train)
+dim(hydro_train)
+length(X_t[1:(length(X_t)-lagmaks)])
 for (i in 1:lagtjek) {
   #setup model
   if (i < lagtjek) {
@@ -225,9 +301,11 @@ for (i in 1:lagtjek) {
 }
 summary(best_adl_aic)
 
-
-
-
+# price lag c(1, 3,  6, 9, 13, 14, 19, 22)
+#hydro lag c(4)
+# con lag c(1,2,10,11,15,16)
+# temp lag c(1,2,17,23)
+# rain lag c(2,5,6)
 
 
 
